e2e4ee7f577f6c7b3a1dcbdb84e0bb67fccf5347
Vipershroom <undertalefan812@gmail.com>
2023-08-05 15:08:35 -0400

    Add eslint & prettier config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  return <div>
-    
-  </div>;
-};
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
 
-export default App;
+export default App
